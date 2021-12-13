f6d14aa2ecb4e2a673432f249a71c7a705e7f7b8
Karamilllka <karamilllka@gmail.com>
2021-12-13 18:23:06 +0500

    add test-1

---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,94 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class MoneyTransferTest {
+    @Test
+    public void shouldTransferFromFirstToSecond() {
+        int amount = 1;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        int balanceOfFirstCardBefore = DashboardPage.getCurrentBalanceOfFirstCard();
+        int balanceOfSecondCardBefore = DashboardPage.getCurrentBalanceOfSecondCard();
+        val transferPage = dashboardPage.secondCard();
+        val cardInfo = DataHelper.getFirstCardInfo();
+        transferPage.makeTransfer(amount, cardInfo);
+        int balanceAfterTransferFirstCard = DataHelper.balanceOfSecondCardAfterTransfer(balanceOfSecondCardBefore, amount);
+        int balanceAfterTransferSecondCard = DataHelper.balanceOfFirstCardAfterTransfer(balanceOfFirstCardBefore, amount);
+        int balanceOfFirstCardAfter = DashboardPage.getCurrentBalanceOfSecondCard();
+        int balanceOfSecondCardAfter = DashboardPage.getCurrentBalanceOfFirstCard();
+        assertEquals(balanceAfterTransferFirstCard, balanceOfFirstCardAfter);
+        assertEquals(balanceAfterTransferSecondCard, balanceOfSecondCardAfter);
+    }
+
+    @Test
+    public void shouldTransferFromSecondToFirst() {
+        int amount = 1;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        int balanceOfFirstCardBefore = DashboardPage.getCurrentBalanceOfFirstCard();
+        int balanceOfSecondCardBefore = DashboardPage.getCurrentBalanceOfSecondCard();
+        val transferPage = dashboardPage.firstCard();
+        val cardInfo = DataHelper.getSecondCardInfo();
+        transferPage.makeTransfer( amount, cardInfo);
+        int balanceAfterTransferFirstCard = DataHelper.balanceOfSecondCardAfterTransfer(balanceOfFirstCardBefore, amount);
+        int balanceAfterTransferSecondCard = DataHelper.balanceOfFirstCardAfterTransfer(balanceOfSecondCardBefore, amount);
+        int balanceOfFirstCardAfter = DashboardPage.getCurrentBalanceOfFirstCard();
+        int balanceOfSecondCardAfter = DashboardPage.getCurrentBalanceOfSecondCard();
+        assertEquals(balanceAfterTransferFirstCard, balanceOfFirstCardAfter);
+        assertEquals(balanceAfterTransferSecondCard, balanceOfSecondCardAfter);
+    }
+
+    @Test
+    void shouldNotTransferMoreThanRestOfBalance() {
+        int amount = 11000;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        DashboardPage.getCurrentBalanceOfFirstCard();
+        DashboardPage.getCurrentBalanceOfSecondCard();
+        val transferPage = dashboardPage.firstCard();
+        val cardInfo = DataHelper.getSecondCardInfo();
+        transferPage.makeTransfer(amount, cardInfo);
+        $(withText("Вы ввели сумму, превышающую остаток средств на Вашем счете. Пожалуйста, введите другую сумму")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTransferOfZero() {
+        int amount = 0;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        DashboardPage.getCurrentBalanceOfFirstCard();
+        DashboardPage.getCurrentBalanceOfSecondCard();
+        val transferPage = dashboardPage.firstCard();
+        val cardInfo = DataHelper.getSecondCardInfo();
+        transferPage.makeTransfer(amount, cardInfo);
+        $(withText("Пожалуйста, укажите сумму, которую необходимо ввести")).shouldBe(Condition.visible);
+    }
+}
+
+
+
